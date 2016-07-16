c0d51c98acc6e3f8e4b7216146284cb2f5a9be0f
katahide100 <katahide100@gmail.com>
2016-07-16 14:31:55 +0900

    node.jsを使ったリアルタイムシステム用のテストスクリプト追加

---
+++ chat2/main.js
@@ -0,0 +1,8 @@
+var Vue = require('vue')
+console.log('tuuka');
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello,Vue.js!'
+  }
+})
---
+++ src/public/js/vue/main.js
@@ -0,0 +1,8 @@
+var Vue = require('vue')
+console.log('tuuka22');
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello,Vue.js!'
+  }
+})
