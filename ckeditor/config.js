83f1a0d32a9efc976ebc29fa351012736756b90f
kataoka hidemasa <katahide100@gmail.com>
2017-08-14 11:45:52 +0000

    ckeditorのフォントサイズ設定変更

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -12,6 +12,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.basicEntities = false;
 	config.entities = false;
 	config.allowedContent = true;
+	config.fontSize_sizes = '5/5px;6/6px;7/7px;8/8px;9/9px;10/10px;11/11px;12/12px;13/13px;14/14px;15/15px;16/16px;17/17px;18/18px;19/19px;20/20px;21/21px;22/22px;23/23px;24/24px;25/25px;26/26px;27/27px;28/28px;29/29px;30/30px;36/36px;48/48px;72/72px;';
 	//config.fullPage = true;
 	config.contentsCss = '../../css/help.css';
 	config.filebrowserImageBrowseUrl = '../../kcfinder/browse.php?type=images';
