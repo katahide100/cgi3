50b363e9fa63106a5c1b354d021101773f711ac4
片岡秀昌 <kataokahidemasa@kataokahidemasa-no-MacBook-Pro.local>
2018-09-30 22:42:15 +0900

    寄付のお願い追加

---
+++ etc/info.php
@@ -0,0 +1,124 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
+	"http://www.w3.org/TR/REC-html40/loose.dtd">
+<html lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<title>デュエル・マスターズ対戦CGIお知らせ</title>
+<style type="text/css"><!-- 
+	body	{ background-color: #fffff0; font-size: small; }
+	.section{
+		width: 90%;
+		background-color: #ffffff;
+		margin: 2em auto;
+		padding: 1em;
+		border: 1px solid #999999;
+	}
+	ul		{ margin: 1.5em 2em; }
+	li		{ line-height: 1.5; margin-bottom: 0.3em; }
+	p,h2,h3,h4,dt
+			{ margin: 1em 2em ; line-height: 1.5; }
+	dd		{ margin: 0 3em 1em 3em ; line-height: 1.5; }
+	dd p	{ margin: 1em 0; }
+	em		{ color: red; font-style: normal; }
+	h1		{ text-align: center; }
+	hr		{ width: 90%; height: 1px; }
+	img		{ margin: 10px; }
+ --></style>
+</head>
+<body>
+<div class="section">
+<h1>デュエル・マスターズ対戦CGI　お知らせ</h1>
+<hr>
+<p>
+いつもデュエルマスターズ対戦CGI	をご利用いただき、ありがとうございます。</br>
+当サイトにおきましては、全機能を無料でご利用いただけます。</br></br>
+ただ、運営していくにあたり、運用費、開発費がやはり必要ということもあり、寄付も募らせていただいております。</br>
+現在も、少しずつでも継続してご寄付いただいている方もいらっしゃり、大変ありがたく思っております。</br></br>
+管理人の私的な理由もあり申し訳ございませんが、今後運用、開発をスムーズに進める上で、さらなるご支援をいただけると大変助かるということと、ご寄付いただいた方への謝礼も含めまして、下記期間中、ご寄付いただいた額に応じて、オリカ券を配布させていただくことと致しました。(配布量調整の関係で少し高めに設定させていただいておりますことご了承ください。)</br>
+今後も当サイトを継続してご利用いただけるよう、ご支援いただけると幸いです。</br>
+また、開発におきましても、ご期待に添えるよう頑張っていまいりたいと思っております。</br>
+何卒、よろしくお願い致します。</br></br>
+</br>
+◆実施期間
+<table border="1">
+<tr>
+<td>
+2018/10/1〜10/12
+</td>
+</tr>
+</table>
+</br>
+◆配布について
+<table border="1">
+<tr>
+<th>
+配布枚数と寄付額について
+</th>
+<td>
+<font size=2>※1口=1000円</font><br><br>
+1枚目=1口<br>
+2枚目=4口<br>
+3,4,5枚目=5口<br>
+6,7枚目=10口<br>
+</td>
+</tr>
+<tr>
+<th>
+1ユーザーへの最大配布量
+</th>
+<td>
+7口</br>
+<font size=2>※配布量調整のためご了承ください。</font>
+</td>
+</tr>
+<tr>
+<th>
+寄付方法
+</th>
+<td>
+下記ページの、「銀行振込での寄付」にてお願い致します。<br>
+<a href="http://www11428uo.sakura.ne.jp/cgi3/donation/">寄付のお願い</a>
+</td>
+</tr>
+<tr>
+<th>
+配布終了条件
+</th>
+<td>
+期間終了もしくは、合計金額が10万円到達した時点まで。<br>
+合計金額に到達した場合は、管理人が対戦画面のチャットでお知らせいたします。<br>
+<font size=2>※お知らせが来てから寄付いただいたものに関しては申し訳ございませんが、配布対象外とさせていただきます。</font></br>
+</td>
+</tr>
+<tr>
+<th>
+お願い
+</th>
+<td>
+寄付いただきましたら、下記アドレスに<font color="red">必ず</font>「ユーザーID（「XXX88888」形式のもの）」と「寄付額」をメールいただきますようお願い致します。<br>
+どうしてもメールが難しい場合は、CGIのチャットの方でご連絡をお願い致します。<br>
+<br>
+メールアドレス：katahide100@gmail.com<br>
+<br>
+例)<br>
+件名：寄付について<br>
+内容：<br>
+&nbsp;&nbsp;ユーザーID：XXX88888<br>
+&nbsp;&nbsp;寄付額：1000円<br>
+<font size=2 color="red">※上記でご連絡いただけない場合、配布が難しいため、必ずご連絡いただきますようお願い致します。</font>
+</td>
+</tr>
+</table>
+</br>
+
+</div>
+
+<p> </p>
+
+<p> </p>
+
+<p> </p>
+
+</body>
+</html>
